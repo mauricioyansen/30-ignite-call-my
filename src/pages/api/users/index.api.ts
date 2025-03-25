5b6dd6258874d263919f7ffbb23fc5fbf4ffc1e0
Mauricio <mauricio_cy@hotmail.com>
2025-03-25 19:46:40 -0300

    posting user and setting cookie

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { setCookie } from "nookies";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { name, username } = req.body;
+
+  const userExists = await prisma.user.findUnique({
+    where: { username },
+  });
+
+  if (userExists)
+    return res.status(400).json({ message: "Username already exists" });
+
+  const user = await prisma.user.create({
+    data: { name, username },
+  });
+
+  setCookie({ res }, "@ignite-call:userId", user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
+    path: "/",
+  });
+
+  return res.status(201).json(user);
+}
