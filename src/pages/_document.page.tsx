f699d0d7d4477d4f2e5949fee39caa3271442f09
Mauricio <mauricio_cy@hotmail.com>
2025-03-25 17:18:07 -0300

    hero section

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -15,7 +15,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText }} />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
       <body>
         <Main />
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Hero, Preview } from "./styles";
+import previewImg from "@/assets/01.png";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImg}
+          alt="calendar"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  );
+}
