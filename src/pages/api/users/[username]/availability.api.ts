18324aac0fe6ba32a0bb2bb131b8e1c5eec1181c
Mauricio <mauricio_cy@hotmail.com>
2025-03-27 19:04:45 -0300

    schedules availabilities

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,64 @@
+import { prisma } from "@/lib/prisma";
+import dayjs from "dayjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") return res.status(405).end();
+
+  const username = String(req.query.username);
+  const { date } = req.query;
+
+  if (!date) return res.status(400).json({ message: "Date not provided" });
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  });
+
+  if (!user) return res.status(400).json({ message: "User doesn't exist" });
+
+  const referenceDate = dayjs(String(date));
+  const isPastDate = referenceDate.endOf("day").isBefore(new Date());
+
+  if (isPastDate) return res.json({ possibleTimes: [], availableTimes: [] });
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      weekDay: referenceDate.get("day"),
+    },
+  });
+
+  if (!userAvailability)
+    return res.json({ possibleTimes: [], availableTimes: [] });
+
+  const { timeEndInMinutes, timeStartInMinutes } = userAvailability;
+  const startHour = timeStartInMinutes / 60;
+  const endHour = timeEndInMinutes / 60;
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i;
+    }
+  );
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set("hour", startHour).toDate(),
+        lte: referenceDate.set("hour", endHour).toDate(),
+      },
+    },
+  });
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time
+    );
+  });
+
+  return res.json({ possibleTimes, availableTimes });
+}
