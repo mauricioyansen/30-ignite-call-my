8655758b8221659bc8d0899a77446172ff4ae880
Mauricio <mauricio_cy@hotmail.com>
2025-03-27 20:47:03 -0300

    switching db for mysql and getting blocked scheduling dates

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -10,6 +10,9 @@ import {
 import { getWeekDays } from "@/utils/get-week-days";
 import { useMemo, useState } from "react";
 import dayjs from "dayjs";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/axios";
+import { useRouter } from "next/router";
 
 interface CalendarWeek {
   week: number;
@@ -26,10 +29,16 @@ interface CalendarProps {
   onDateSelected: (date: Date) => void;
 }
 
+interface BlockedDates {
+  blockedWeekDays: number[];
+}
+
 export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set("date", 1);
   });
+
+  const router = useRouter();
 
   function handlePreviousMonth() {
     const previousMonthDate = currentDate.subtract(1, "month");
@@ -48,7 +57,28 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const currentMonth = currentDate.format("MMMM");
   const currentYear = currentDate.format("YYYY");
 
+  const username = String(router.query.username);
+
+  const { data: blockedDates } = useQuery<BlockedDates>({
+    queryKey: [
+      "blocked-dates",
+      currentDate.get("year"),
+      currentDate.get("month"),
+    ],
+    queryFn: async () => {
+      const response = await api.get(`/users/${username}/blocked-dates`, {
+        params: {
+          year: currentDate.get("year"),
+          month: currentDate.get("month"),
+        },
+      });
+      return response.data;
+    },
+  });
+
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) return [];
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -83,7 +113,12 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
         return { date, disabled: true };
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: date.endOf("day").isBefore(new Date()) };
+        return {
+          date,
+          disabled:
+            date.endOf("day").isBefore(new Date()) ||
+            blockedDates.blockedWeekDays.includes(date.get("day")),
+        };
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true };
@@ -107,7 +142,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
     );
 
     return calendarWeeks;
-  }, [currentDate]);
+  }, [currentDate, blockedDates]);
 
   console.log(calendarWeeks);
 
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,10 +6,11 @@ import {
   TimePickerItem,
   TimePickerList,
 } from "./styles";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import dayjs from "dayjs";
 import { api } from "@/lib/axios";
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
 
 interface Availability {
   possibleTimes: number[];
@@ -18,7 +19,6 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [availability, setAvailability] = useState<Availability | null>(null);
 
   const router = useRouter();
 
@@ -31,17 +31,20 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format("DD[ de ]MMMM")
     : null;
 
-  useEffect(() => {
-    if (!selectedDate) return;
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format("YYYY-MM-DD")
+    : null;
 
-    api
-      .get(`/users/${username}/availability`, {
-        params: { date: dayjs(selectedDate).format("YYYY-MM-DD") },
-      })
-      .then((response) => {
-        setAvailability(response.data);
+  const { data: availability } = useQuery<Availability>({
+    queryKey: ["availability", selectedDateWithoutTime],
+    queryFn: async () => {
+      const response = await api.get(`/users/${username}/availability`, {
+        params: { date: selectedDateWithoutTime },
       });
-  }, [selectedDate, username]);
+      return response.data;
+    },
+    enabled: !!selectedDate,
+  });
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
