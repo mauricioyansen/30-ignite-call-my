a5ee5a0e0d71a832d80063f4ac16769ab490daa4
Mauricio <mauricio_cy@hotmail.com>
2025-03-28 10:58:16 -0300

    creating an scheduling

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { CalendarStep } from "./CalendarStep";
 import { ConfirmStep } from "./ConfirmStep";
 
 export function ScheduleForm() {
-  return (
-    <CalendarStep/>
-    // <ConfirmStep />
-  );
+  const [selectedDateTime, setSelectDateTime] = useState<Date | null>();
+
+  function handleClearSelectedDateTime() {
+    setSelectDateTime(null);
+  }
+
+  if (selectedDateTime)
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    );
+
+  return <CalendarStep onSelectDateTime={setSelectDateTime} />;
 }
