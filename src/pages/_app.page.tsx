aed5d8edc50355722c7a1afa0771f1a029b44049
Mauricio <mauricio_cy@hotmail.com>
2025-03-28 11:58:24 -0300

    SEO on main pages

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/react-query";
+import { DefaultSeo } from "next-seo";
 
 globalStyles();
 
@@ -14,6 +15,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: "website",
+            locale: "pt_BR",
+            url: "https://ignite-call-my.com",
+            siteName: "Ignite Call",
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,29 +3,37 @@ import { Container, Hero, Preview } from "./styles";
 import previewImg from "@/assets/01.png";
 import Image from "next/image";
 import { ClaimUsernameForm } from "./components/ClaimUsernameForm";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-        <ClaimUsernameForm />
-      </Hero>
+          <ClaimUsernameForm />
+        </Hero>
 
-      <Preview>
-        <Image
-          src={previewImg}
-          alt="calendar"
-          height={400}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+        <Preview>
+          <Image
+            src={previewImg}
+            alt="calendar"
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   );
 }
