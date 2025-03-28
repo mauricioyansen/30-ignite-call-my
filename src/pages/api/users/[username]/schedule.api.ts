3cf540f81d5d931147eb579e2b2ffaf5d81f115e
Mauricio <mauricio_cy@hotmail.com>
2025-03-28 11:40:32 -0300

    creating an event on user's google calendar

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,5 +1,7 @@
+import { getGoogleAuthToken } from "@/lib/google";
 import { prisma } from "@/lib/prisma";
 import dayjs from "dayjs";
+import { google } from "googleapis";
 import { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 
@@ -47,13 +49,38 @@ export default async function handle(
     });
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       notes,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  });
+
+  const calendar = google.calendar({
+    version: "v3",
+    auth: await getGoogleAuthToken(user.id),
+  });
+
+  await calendar.events.insert({
+    calendarId: "primary",
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: notes,
+      start: { dateTime: schedulingDate.format() },
+      end: { dateTime: schedulingDate.add(1, "hour").format() },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: "hangoutsMeet",
+          },
+        },
+      },
     },
   });
 
